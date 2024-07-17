4377aedba79911e2e1180eb4da84fd1df78c043c
mkutu <mattkutugata@gmail.com>
2024-07-17 18:02:45 -0400

    house keeping for setting up new directories in data

--- src/export_blob_metrics.py
+++ src/export_blob_metrics.py
@@ -20,7 +20,8 @@ class ExporterBlobMetrics:
         Initializes the BlobMetricExporter with configuration data.
         """
         self.__auth_config_data = read_yaml(cfg.paths.pipeline_keys)
-        self.output_dir = cfg.paths.data_dir
+        self.output_dir = Path(cfg.paths.data_dir, "blob_containers")
+        self.output_dir.mkdir(exist_ok=True, parents=True)
         
     def run_azcopy_ls(self) -> None:
         """
