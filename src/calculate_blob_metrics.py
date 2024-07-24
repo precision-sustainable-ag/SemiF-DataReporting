2d56543c18b3848001e3f597212b7413e4ce5947
hirunima <39465010+hirunima@users.noreply.github.com>
2024-07-24 16:40:27 -0400

    creating the pdf table for batch statistics and add list of unprocessed batches

--- src/calculate_blob_metrics.py
+++ src/calculate_blob_metrics.py
@@ -22,7 +22,7 @@ def calculate_average_image_counts(image_counts: pd.DataFrame) -> pd.DataFrame:
     return average_image_counts
 
 #Function to compare file type lenghts
-def compute_matching( df: pd.DataFrame, matching_folders: str) -> dict:
+def compute_matching( df: pd.DataFrame, matching_folders: str) -> tuple[dict, list]  :
     pd.set_option('display.max_colwidth', None)
     print(df.head(10))
      
@@ -30,7 +30,7 @@ def compute_matching( df: pd.DataFrame, matching_folders: str) -> dict:
     grouped_df = grouped_df.groupby(['Batch','FileName'])
     log.info(f"Extracted DataFrame with {len(grouped_df)} rows for matching.")
     # Check for mismatch files
-    batch_stat={x: {'images':0,'metadata':0,'meta_mask':0,'isMatching':True,'Missing':[]} for x in df.Batch.unique()}
+    batch_stat={x: {'images':0,'metadata':0,'meta_mask':0,'isMatching':'True','Missing':[]} for x in df.Batch.unique()}
     for key, item in grouped_df:
         if len(item) < 4:
             # assert len(item) == 0, f"{key} does not have any files."
@@ -52,14 +52,24 @@ def compute_matching( df: pd.DataFrame, matching_folders: str) -> dict:
                 else:
                     #create a list for missing files ignore unprocessed folders
                     batch_stat[key[0]]['Missing'].append(f"{item['FileName'].iloc[0]}.{expected_file}")
-            batch_stat[key[0]]['isMatching'] = False
+            batch_stat[key[0]]['isMatching'] = 'False'
             
         else:
             batch_stat[key[0]]['images'] += 1
             batch_stat[key[0]]['metadata'] += 1
             batch_stat[key[0]]['meta_mask'] += 1
+
+    #creating a seperate list for unproceesd batches
+    unprocessed_batches = []
+    for i in batch_stat:
+        if batch_stat[i]['metadata']==0 and batch_stat[i]['images']!=0 and batch_stat[i]['meta_mask']==0:
+            unprocessed_batches.append(i)
+            batch_stat[i]['Missing'] = ['Unprossed Batch'] 
+            # uncomment to delete the key from the batch statistics
+            # del batch_stat[i]   
+           
     
-    return batch_stat
+    return batch_stat,unprocessed_batches
 
 
 
--- src/report_blob_metrics.py
+++ src/report_blob_metrics.py
@@ -37,12 +37,22 @@ class ReporterBlobMetrics:
                 batch = parts[0].replace('INFO: ', '').strip()
             else:
                 batch = parts[0]
+            
             full_filename = parts[-1].split(";")[0].strip()
             folder_name=parts[-2]
             
             if '.' in full_filename:
                 file_type = full_filename.split('.')[-1]
-                file_name=full_filename.split('.')[-2]
+                #hardcord to remove MD_2022-06-22
+                if batch=='MD_2022-06-22' and file_type in ['json',]and len(full_filename.split('.'))>2:
+                    continue
+                    # rearrange=full_filename.split('.')[-3].split('_')
+                    # file_name='MD_Row-'+rearrange[1]+'_'+rearrange[-1]
+                # elif batch=='MD_2022-06-22' and file_type in ['jpg']:
+                #     rearrange=full_filename.split('.')[-3].split('_')
+                #     file_name='MD_Row-'+rearrange[1]+'_'+rearrange[-1]
+                else:
+                    file_name=full_filename.split('.')[-2]
             else:
                 file_type = 'folder'
                 file_name=full_filename
@@ -50,6 +60,13 @@ class ReporterBlobMetrics:
             batches.append((batch, folder_name, file_name, file_type))
         log.info(f"Extracted {len(batches)} batches.")
         return batches
+    
+    def name_diferences(self,file_name: str, folder_name: str) -> str:
+        #rearranges the file name to match the image name
+        w=1
+        return None
+
+
     
     def remove_invalid_batches(self, df: pd.DataFrame, column_name: str) -> pd.DataFrame:
         # Removes invalid batches from DataFrame.
@@ -88,16 +105,51 @@ class ReporterBlobMetrics:
         return result_df
 
     def generate_pdf_report(self, result_df: pd.DataFrame, output_path: Path) -> None:
+        
         # Combines image counts and average image counts into a single DataFrame.
         pdf = FPDF()
+        # pdf.add_page()
+        # pdf.set_font("Arial", size=12)
+        # pdf.cell(200, 10, txt="Image Counts and Averages Report", ln=True, align='C')
+        # for index, row in result_df.iterrows():
+        #     pdf.cell(200, 5, txt=f"{row['Batch']}", ln=True)
+        #     pdf.cell(200, 5, txt=f"Image Count: {row['ImageCount']}", ln=True)
+        #     pdf.cell(200, 5, txt=f"Average Image Count: {row['AverageImageCount']:.2f}", ln=True)
+        #     pdf.ln(10)
+
         pdf.add_page()
-        pdf.set_font("Arial", size=12)
-        pdf.cell(200, 10, txt="Image Counts and Averages Report", ln=True, align='C')
-        for index, row in result_df.iterrows():
-            pdf.cell(200, 5, txt=f"{row['Batch']}", ln=True)
-            pdf.cell(200, 5, txt=f"Image Count: {row['ImageCount']}", ln=True)
-            pdf.cell(200, 5, txt=f"Average Image Count: {row['AverageImageCount']:.2f}", ln=True)
-            pdf.ln(10)
+        pdf.set_font("Times", size=10)
+        line_height = pdf.font_size
+        col_width = pdf.epw /9
+
+        #one liner for removing lengthy lists in the last cell to first three files.
+        result_df = result_df.map(lambda y: y[:3]+['...',] if isinstance(y, list) and len(y)>3 else y )
+        
+        logging.info(f"Creating the batch statistics report.")
+        #create your fpdf table ..
+        for j,row in result_df.iterrows():
+            #add the column names
+            if j==0:
+                for header in result_df.columns:
+                    pdf.cell(col_width, line_height, str(header), border=1)
+                pdf.ln(line_height)
+            #choose right height for current row
+            if len(row[5])>1:
+                line_height=pdf.font_size*(len(row[5]))
+            else:
+                line_height=pdf.font_size
+            #draw each cell in the row
+            for i,datum in enumerate(row):                    
+                if i== 5:
+                    pdf.multi_cell(col_width*3, line_height, str(datum), border=1,align='L',ln=3, 
+                max_line_height=pdf.font_size)
+                elif i==0:
+                    pdf.multi_cell(col_width*2, line_height, str(datum), border=1,align='L',ln=3, max_line_height=pdf.font_size)
+                else:
+                    pdf.multi_cell(col_width, line_height, str(datum), border=1,align='L',ln=3, max_line_height=pdf.font_size)
+                
+            pdf.ln(line_height)
+
         pdf.output(output_path)
         log.info(f"PDF report generated and saved to {output_path}.")
 
@@ -113,6 +165,7 @@ def main(cfg: DictConfig) -> None:
     
     # Main execution
     batches = reporter.extract_batches(lines)
+
     df = pd.DataFrame(batches, columns=['Batch', 'FolderName', 'FileName', 'FileType'])
     log.info(f"Created DataFrame with {len(df)} rows.")
 
@@ -123,27 +176,26 @@ def main(cfg: DictConfig) -> None:
     average_image_counts = calculate_blob_metrics.calculate_average_image_counts(image_counts)
 
     #Compare file type lengths
-    mismatch_statistics=calculate_blob_metrics.compute_matching(df_filtered,cfg.matching_folders)
+    mismatch_statistics,unprocessed_batches=calculate_blob_metrics.compute_matching(df_filtered,cfg.matching_folders)
     #writing the mismatch statistics to a csv file for now
-    
-
-    # with open('mismatch_statistics_record.csv', 'w') as csvfile:
-    #     writer = csv.DictWriter(csvfile, fieldnames=list(mismatch_statistics.keys()))
-    #     writer.writeheader()
-    #     writer.writerow(mismatch_statistics)
 
     fields=['Batch','images','metadata','meta_mask','isMatching','Missing']
+    frames = []
     with open('mismatch_statistics_record.csv', 'w') as csvfile:
         w = csv.DictWriter( csvfile, fields )
         for key,val in sorted(mismatch_statistics.items()):
-            # import pdb; pdb.set_trace()
             row = {'Batch': key}
             row.update(val)
+            
+            frames.append(pd.DataFrame([row]))
             w.writerow(row)
-
-    result_df = reporter.combine_data(image_counts, average_image_counts)
+    
+    # result_df = reporter.combine_data(image_counts, average_image_counts)
     output_path = Path(reporter.report_dir,'semifield-developed-images_image_counts_and_averages_report.pdf')
-    reporter.generate_pdf_report(result_df, output_path)
+    
+    pdf_df=pd.concat(frames).reset_index(drop=True)
+    pdf_df.columns=fields
+    reporter.generate_pdf_report(pdf_df, output_path)
 
     log.info(f"{cfg.task} completed.")
 
