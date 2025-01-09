03e8bd5767f8f6204d8fde2f97fe9384209784c6
Jinam Shah <jinams47@gmail.com>
2025-01-09 20:03:38 +0530

    added summary report + plot

--- src/report.py
+++ src/report.py
@@ -22,19 +22,20 @@ class Report:
         self.source_location = cfg.paths.data_dir
         self.parent_report_folder = cfg.paths.report
         self.report_folder = None
-        self.message_blocks = [
-            {
-                'type': 'section',
-                'text': {
-                    'type': 'mrkdwn',
-                    'text': f"*Data Report* - {datetime.now().strftime('%m/%d/%Y')}"
-                }
-            },
-            {
-                'type': 'divider'
-            }
-        ]
-        self.files = []
+        self.summary_stats = {}
+        # self.message_blocks = [
+        #     {
+        #         'type': 'section',
+        #         'text': {
+        #             'type': 'mrkdwn',
+        #             'text': f"*Data Report* - {datetime.now().strftime('%m/%d/%Y')}"
+        #         }
+        #     },
+        #     {
+        #         'type': 'divider'
+        #     }
+        # ]
+        # self.files = []
 
     def copy_relevant_files(self):
         # get relevant blobcontainers files
@@ -81,30 +82,30 @@ class Report:
                      os.path.join(self.report_folder,
                                   f'semif_developed_batch_details_lts.csv'))
 
-    def compose_slack_message(self, actionable_summary):
-        # with open(os.path.join(self.report_folder, 'semif-HighLevelStats.txt'),
-        #           'r') as f:
-        #     self.message_blocks.append({
-        #         'type': 'section',
-        #         'text': {
-        #             'type': 'mrkdwn',
-        #             'text': ''.join(f.readlines())
-        #         }
-        #     })
-        self.message_blocks.append({
-            'type': 'section',
-            'text': {
-                'type': 'mrkdwn',
-                'text': actionable_summary
-            }
-        })
-        self.files.append(os.path.join(self.report_folder,
-                                       'actionable_items.csv'))
-        # for file in os.listdir(self.report_folder):
-        #     if file.lower().endswith('.png'):
-        #         self.files.append(os.path.join(self.report_folder, file))
+    # def compose_slack_message(self, actionable_summary):
+    #     # with open(os.path.join(self.report_folder, 'semif-HighLevelStats.txt'),
+    #     #           'r') as f:
+    #     #     self.message_blocks.append({
+    #     #         'type': 'section',
+    #     #         'text': {
+    #     #             'type': 'mrkdwn',
+    #     #             'text': ''.join(f.readlines())
+    #     #         }
+    #     #     })
+    #     self.message_blocks.append({
+    #         'type': 'section',
+    #         'text': {
+    #             'type': 'mrkdwn',
+    #             'text': actionable_summary
+    #         }
+    #     })
+    #     self.files.append(os.path.join(self.report_folder,
+    #                                    'actionable_items.csv'))
+    #     # for file in os.listdir(self.report_folder):
+    #     #     if file.lower().endswith('.png'):
+    #     #         self.files.append(os.path.join(self.report_folder, file))
 
-    def send_slack_notification(self):
+    def send_slack_notification(self, message_blocks, files):
         client = WebClient(token=self.__auth_config_data['slack_api_token'])
         # message = {
         #     "channel": "#semif-datareports",
@@ -128,10 +129,9 @@ class Report:
         #         },
         #     ]
         # }
-        log.info(self.message_blocks)
         message = {
             'channel': self.task_cfg.slack_channel,
-            'blocks': self.message_blocks
+            'blocks': message_blocks
         }
         try:
             message_response = client.chat_postMessage(**message)
@@ -139,7 +139,7 @@ class Report:
             log.info(
                 f"sent slack message to channel - {message_response['channel']}, thread - {message_response['ts']}")
 
-            for file in self.files:
+            for file in files:
                 file_response = client.files_upload_v2(
                     channel=message_response['channel'],
                     file=file,
@@ -291,6 +291,44 @@ class Report:
         combined_cutouts_df = combined_cutouts_df.replace(np.nan, None)
         return combined_cutouts_df, lts_cutouts_duplicated_batches
 
+    def _generate_summary_stats(self, uploads_df, developed_df, cutouts_df):
+        actionable_df = pd.read_csv(os.path.join(self.report_folder,
+                                                 'actionable_items.csv'),
+                                    index_col=0)
+        processed_counts = pd.crosstab(actionable_df['state'],
+                                       actionable_df['processed'])
+        # processed_counts.plot(kind='bar', width=0.8)
+        ax = processed_counts.plot(kind='bar', stacked=False, figsize=(10, 6))
+        plt.title(f'Processed vs Unprocessed batches by State')
+        plt.xlabel('State')
+        plt.ylabel('Count')
+        plt.legend(title='Processed')
+        plt.tight_layout()
+        plt.savefig(
+            os.path.join(self.report_folder, f'processed_by_states.png'),
+            dpi=300)
+        self.summary_stats['files'] = [os.path.join(self.report_folder,
+                                                    f'processed_by_states.png')]
+        self.summary_stats['totalSizeTB'] = {
+            'uploads': (uploads_df['totalSizeGiB_lts'].fillna(
+                uploads_df['totalSizeGiB_az']).sum()) / 1024,
+            'developed': (developed_df['ImagesFolderSizeGiB_lts'].fillna(
+                developed_df['ImagesFolderSizeGiB_az']).sum() +
+                          developed_df['MetadataFolderSizeGiB_lts'].fillna(
+                              developed_df['MetadataFolderSizeGiB_az']).sum() +
+                          developed_df['MetaMasksFolderSizeGiB_lts'].fillna(
+                              developed_df['MetaMasksFolderSizeGiB_az']).sum()
+                          ) / 1024,
+            'cutouts': (cutouts_df['jpg_size_gib_lts'].fillna(
+                cutouts_df['jpg_size_gib_az']).sum() +
+                        cutouts_df['png_size_gib_lts'].fillna(
+                            cutouts_df['png_size_gib_az']).sum() +
+                        cutouts_df['json_size_gib_lts'].fillna(
+                            cutouts_df['json_size_gib_az']).sum() +
+                        cutouts_df['mask_size_gib_lts'].fillna(
+                            cutouts_df['mask_size_gib_az']).sum()) / 1024
+        }
+
     def generate_actionable_table(self):
         # current implementation ignores duplicates, also has extra variable declarations
         # optimize + update implementation later
@@ -427,31 +465,113 @@ class Report:
         pd.DataFrame(records).to_csv(
             os.path.join(self.report_folder, f"actionable_items.csv"))
 
+        self._generate_summary_stats(uploads_df, developed_df, cutouts_df)
         # TODO: deal with duplicate records
         return
 
-    def generate_actionable_summary(self):
+    def generate_actionable_message(self):
+        message_blocks = [
+            {
+                'type': 'section',
+                'text': {
+                    'type': 'mrkdwn',
+                    'text': f"*SemiF - Actionable items Report* - "
+                            f"{datetime.now().strftime('%m/%d/%Y')}"
+                }
+            },
+            {
+                'type': 'divider'
+            }
+        ]
         actionable_df = pd.read_csv(
             os.path.join(self.report_folder, 'actionable_items.csv'),
             index_col=0)
-        preprocessed_text = f"There are " \
-                            f"{actionable_df['preprocessed'].eq(False).sum()}" \
-                            f" batches that have not been pre-processed." \
-                            f"\nAlso, preprocessing info is " \
-                            f"missing for " \
-                            f"{actionable_df['preprocessed'].isnull().sum()}" \
-                            f" batches."
+        uploads_az_not_lts = actionable_df[(actionable_df['upload_azure']) & (
+                actionable_df['upload_lts'] == False)].shape[0]
+        developed_az_not_lts = actionable_df[
+            (actionable_df['developed_azure']) & (
+                    actionable_df['developed_lts'] == False)].shape[0]
+        cutouts_az_not_lts = actionable_df[(actionable_df['cutouts_azure']) & (
+                actionable_df['cutouts_lts'] == False)].shape[0]
+        message_blocks.extend([
+            {
+                'type': 'section',
+                'text': {
+                    'type': 'mrkdwn',
+                    'text': f"Not preprocessed: "
+                            f"*{actionable_df['preprocessed'].eq(False).sum()}*"
+                            f" batches.\n"
+                            f"Missing preprocessing info: "
+                            f"*{actionable_df['preprocessed'].isnull().sum()}* "
+                            f"batches"
+                },
+            },
+            {
+                'type': 'section',
+                'text': {
+                    'type': 'mrkdwn',
+                    'text': f"Not processed: "
+                            f"*{actionable_df['processed'].eq(False).sum()}* "
+                            f"batches.\n"
+                            f"Missing processing info: "
+                            f"*{actionable_df['processed'].isnull().sum()}* "
+                            f"batches."
 
-        processed_text = f"{actionable_df['processed'].eq(False).sum()} " \
-                         f"batches have not been processed yet." \
-                         f"\nAlso, processing info is missing for " \
-                         f"{actionable_df['processed'].isnull().sum()} batches."
+                },
+            },
+            {
+                'type': 'section',
+                'text': {
+                    'type': 'mrkdwn',
+                    'text': f"*Batches present in AZ but not in LTS:*\n"
+                            f"• semif-uploads: {uploads_az_not_lts}\n"
+                            f"• semif-developed-images: "
+                            f"{developed_az_not_lts}\n"
+                            f"• semif-cutouts: {cutouts_az_not_lts}"
+                },
+            },
+            {
+                'type': 'section',
+                'text': {
+                    'type': 'mrkdwn',
+                    'text': f"Uploaded images: "
+                            f"*{actionable_df['upload_raws'].sum()}*\n"
+                            f"Developed images: "
+                            f"*{actionable_df['developed_jpgs'].sum()}*"
+                },
+            }
+        ])
 
-        az_vs_lts_text = f"Batch present in AZ but not in LTS:\n" \
-                         f"semif-uploads: {actionable_df[(actionable_df['upload_azure']) & (actionable_df['upload_lts'] == False)].shape[0]} \n" \
-                         f"semif-developed-images: {actionable_df[(actionable_df['developed_azure']) & (actionable_df['developed_lts'] == False)].shape[0]} \n" \
-                         f"semif-cutouts: {actionable_df[(actionable_df['cutouts_azure']) & (actionable_df['cutouts_lts'] == False)].shape[0]}"  # nopep8
-        return f"{preprocessed_text}\n{processed_text}\n{az_vs_lts_text}"
+        files = [os.path.join(self.report_folder, 'actionable_items.csv')]
+
+        return message_blocks, files
+
+    def generate_summary_message(self):
+        message_blocks = [
+            {
+                'type': 'section',
+                'text': {
+                    'type': 'mrkdwn',
+                    'text': f"*SemiF - Data Report* - "
+                            f"{datetime.now().strftime('%m/%d/%Y')}"
+                }
+            },
+            {
+                'type': 'divider'
+            }
+        ]
+        md_table = "| Category   | Total Size (TB) |\n" \
+                   "|------------|------------------|\n"
+        for category, size in self.summary_stats['totalSizeTB'].items():
+            md_table += f"| {category.capitalize()} | {round(size, 2)} |\n"
+        message_blocks.append({
+            'type': 'section',
+            'text': {
+                'type': 'mrkdwn',
+                'text': md_table
+            }
+        })
+        return message_blocks, self.summary_stats['files']
 
 
 def main(cfg: DictConfig) -> None:
@@ -460,15 +580,8 @@ def main(cfg: DictConfig) -> None:
 
     report.copy_relevant_files()
     report.generate_actionable_table()
-    actionable_summary = report.generate_actionable_summary()
+    message, files = report.generate_summary_message()
+    report.send_slack_notification(message, files)
 
-    report.compose_slack_message(actionable_summary)
-    report.send_slack_notification()
-    # report._cleanup_lts_uploads_csv()
-    # report.generate_developed_batch_graphs(os.path.join(report.report_folder, 'semif_developed_batch_details_az.csv'))
-    # report.generate_developed_batch_graphs(os.path.join(report.report_folder, 'semif_developed_batch_details_lts.csv'))
-    # log.info('generating report')
-
-    # report.compose_slack_message()
-    # log.info('sending slack notification')
-    # report.send_slack_notification()
+    message, files = report.generate_actionable_message()
+    report.send_slack_notification(message, files)
